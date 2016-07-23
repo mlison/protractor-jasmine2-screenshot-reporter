89c5f72b7c9929c70ccd890ddb126f566c70ad08
Ran Bar-Zik <ran.bar-zik@hpe.com>
2016-07-23 11:19:32 +0300

    Adding real test, modifying before and after hooks

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,59 @@
 'use strict';
 
-var assert = require('assert'),
-  Jasmine2ScreenShotReporter = require('./../index.js');
+var assert = require('chai').assert,
+ expect = require('chai').expect,
+  chai = require('chai'),
+  Jasmine2ScreenShotReporter = require('./../index.js'),
+  rimraf = require('rimraf'),
+  fs = require('fs'),
+  destinationPath = 'test/testdirectory',
+  reportFileName = 'my-custom-report.html';
+
 
 describe('Jasmine2ScreenShotReporter tests', function(){
 
-  it('beforeLaunch should create initial report', function(){
-    var reporter = new Jasmine2ScreenShotReporter();
+  before(function() {
+
+    chai.use(require('chai-fs'));
+
+  });
+
+  it('beforeLaunch should create initial report', function(done){
+    var reporter = new Jasmine2ScreenShotReporter({
+      dest: destinationPath,
+      filename: reportFileName});
 
     assert.equal(typeof reporter.beforeLaunch, 'function'); //Public method beforeLaunch should be defined
+
+    reporter.beforeLaunch(function() {
+      var contents = fs.readFileSync(destinationPath + '/' + reportFileName, 'utf8');
+
+      expect(destinationPath + '/' + reportFileName).to.be.a.path('no path');
+      expect(contents).to.contain('<body><h1>Report</h1>');
+      expect(contents).to.contain('<div id="summary" class="passed"><h4>Summary</h4>' +
+        '<ul>' +
+        '<li id="summaryTotalSpecs">Total specs tested: </li>' +
+        '<li id="summaryTotalFailed">Total failed: </li>' +
+        '</ul></div>');
+      done();
+    });
+
+
   });
+
+
+  after(function(done) {
+    // runs before all tests in this block
+    rimraf(destinationPath, function(err) {
+      if(err) {
+        console.error('Could not delete ' + destinationPath + 'directory')
+      }
+
+      done();
+    });
+
+  });
+
 
 });
 
